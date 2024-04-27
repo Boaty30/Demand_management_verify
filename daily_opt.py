6f0f922b56faa09de51c66c279561762220f8d93
Jizhou Yu <boatyyu@outlook.com>
2024-04-27 23:47:07 +0800

    初步实现日内、日前需量管理目标，仍然有一些小的波动，月度控制思路已有，待实现

--- daily_opt.py
+++ daily_opt.py
@@ -1,11 +1,19 @@
 import cplex
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+from datetime import datetime, timedelta
+from matplotlib.font_manager import FontProperties
+
+def generate_time_labels(num_periods, interval_minutes=15):
+    base_time = datetime(2023, 12, 1)  # 可以是任意日期，只要时间是从0:00开始即可
+    time_labels = [base_time + timedelta(minutes=i * interval_minutes) for i in range(num_periods)]
+    return time_labels
 
 def load_data(file_path):
     # 从CSV文件加载数据
     data = pd.read_csv(file_path)
-    return data['Day1'].values
+    return data['Day5'].values
 
 def create_problem():
     # 初始化一个CPLEX问题实例
@@ -13,16 +21,48 @@ def create_problem():
     prob.objective.set_sense(prob.objective.sense.maximize)
     return prob
 
+
 def add_variables(prob, num_periods, p_max, s_n):
-    # 向问题中添加充电/放电功率变量 P_CS_t 和电池存储状态变量 S_t
+    # 添加充放电功率变量和电池存储状态变量
     for t in range(num_periods):
         prob.variables.add(names=[f"P_CS_{t}"], lb=[-p_max], ub=[p_max])
     prob.variables.add(names=[f"S_{t}" for t in range(num_periods)],
-                       lb=[0.05 * s_n] * num_periods,
-                       ub=[s_n] * num_periods)
-
+                    lb=[0.05 * s_n] * num_periods,
+                    ub=[s_n] * num_periods)
+    # 添加二元控制变量
+    prob.variables.add(names=[f"charge_{t}" for t in range(num_periods)], types=["B"]*num_periods)
+    prob.variables.add(names=[f"discharge_{t}" for t in range(num_periods)], types=["B"]*num_periods)
 
 def add_constraints(prob, num_periods, p_f_max, p_l, s_n, alpha, delta_t):
+    M = 10000  # 大M方法中使用的一个大常数
+    for t in range(num_periods):
+        # 存储状态动态更新
+        if t > 0:
+            prob.linear_constraints.add(
+                lin_expr=[cplex.SparsePair(ind=[f"S_{t-1}", f"S_{t}", f"P_CS_{t-1}"],
+                                           val=[1, -1, alpha * delta_t])],
+                senses=["E"],
+                rhs=[0],
+                names=[f"storage_update_{t}"]
+            )
+
+        # 低于控制线时，禁止放电
+        if p_l[t] < p_f_max:
+            prob.linear_constraints.add(
+                lin_expr=[cplex.SparsePair(ind=[f"P_CS_{t}"], val=[1.0])],
+                senses=["G"],
+                rhs=[0],
+                names=[f"no_discharge_{t}"]
+            )
+        
+        # 高于控制线时，禁止充电
+        if p_l[t] > p_f_max:
+            prob.linear_constraints.add(
+                lin_expr=[cplex.SparsePair(ind=[f"P_CS_{t}"], val=[1.0])],
+                senses=["L"],
+                rhs=[0],
+                names=[f"no_charge_{t}"]
+            )
     # 添加 S_0 的初始存储状态约束
     prob.linear_constraints.add(
         lin_expr=[cplex.SparsePair(ind=["S_0"], val=[1.0])],
@@ -49,6 +89,7 @@ def add_constraints(prob, num_periods, p_f_max, p_l, s_n, alpha, delta_t):
             names=[f"power_limit_{t}"]
         )
 
+
 def set_objective(prob, num_periods, h, w, is_valley):
     # 设置目标函数
     objective = []
@@ -70,72 +111,93 @@ def set_objective(prob, num_periods, h, w, is_valley):
 
 
 
-def solve_problem(file_path, s_n, alpha, delta_t, output_file):
-    # 载入负荷功率数据并设置问题参数
-    p_l = load_data(file_path)
-    num_periods = len(p_l)
-    print(f"Number of periods = {num_periods}")
-    p_f_max = 4500
-    p_max = 900
-    h = 0.25
-    w = 0.05
-    is_valley = [True if (i>=36 and i<=48) or (i>=68 and i<=76) or (i>=80 and i<=88) else False for i in range(num_periods)]
+def solve_problem(file_path, s_n, alpha, delta_t, output_file, p_f_max=4750, figpath="C:\\Codes\\Projects\\Demand_management_verify\\Data\\\power_and_storage_profiles.png"):
+    try:
+        # 载入负荷功率数据并设置问题参数
+        p_l = load_data(file_path)
+        num_periods = len(p_l)
+        print(f"Number of periods = {num_periods}")
+        p_max = 900
+        h = 0.25
+        w = 1
+        is_valley = [True if (i>=36 and i<=48) or (i>=68 and i<=76) or (i>=80 and i<=88) else False for i in range(num_periods)]
 
-    # 创建并设置问题
-    prob = create_problem()
-    add_variables(prob, num_periods, p_max, s_n)
-    add_constraints(prob, num_periods, p_f_max, p_l, s_n, alpha, delta_t)
-    set_objective(prob, num_periods, h, w, is_valley)
+        # 创建并设置问题
+        prob = create_problem()
+        add_variables(prob, num_periods, p_max, s_n)
+        add_constraints(prob, num_periods, p_f_max, p_l, s_n, alpha, delta_t)
+        set_objective(prob, num_periods, h, w, is_valley)
 
-    # 解决问题
-    prob.solve()
+        # 解决问题
+        prob.solve()
 
-    # 输出结果
-    print("Solution status = ", prob.solution.get_status(), ":", prob.solution.status[prob.solution.get_status()])
-    print("Solution value  = ", prob.solution.get_objective_value())
-    for t in range(num_periods):
-        print(f"P_CS_{t} = {prob.solution.get_values(f'P_CS_{t}'): .2f}")
-        print(f"S_{t} = {prob.solution.get_values(f'S_{t}'): .2f}")
+        # 输出结果
+        print("Solution status = ", prob.solution.get_status(), ":", prob.solution.status[prob.solution.get_status()])
+        print("Solution value  = ", prob.solution.get_objective_value())
+        for t in range(num_periods):
+            print(f"P_CS_{t} = {prob.solution.get_values(f'P_CS_{t}'): .2f}")
+            print(f"S_{t} = {prob.solution.get_values(f'S_{t}'): .2f}")
 
-    # 创建一个空的 DataFrame 来存储结果
-    solution_df = pd.DataFrame(columns=['P_CS', 'S'])
-    for t in range(num_periods):
-        p_cs = prob.solution.get_values(f'P_CS_{t}')
-        s = prob.solution.get_values(f'S_{t}')
-        solution_df.loc[t] = [p_cs, s]
+        # 创建一个空的 DataFrame 来存储结果
+        solution_df = pd.DataFrame(columns=['P_CS', 'S'])
+        for t in range(num_periods):
+            p_cs = prob.solution.get_values(f'P_CS_{t}')
+            s = prob.solution.get_values(f'S_{t}')
+            solution_df.loc[t] = [p_cs, s]
 
-    # 将结果保存到 CSV 文件中
-    solution_df.to_csv(output_file, index=False)
+        # 将结果保存到 CSV 文件中
+        solution_df.to_csv(output_file, index=False)
 
-    # 创建一个 Matplotlib Figure，并添加两个子图
-    fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 12))
-    
-    # 在第一个子图上绘制 P_L、P_CS 和 P_CS-P_L 的折线图
-    ax1.plot(p_l, label='P_L')
-    ax1.plot(solution_df['P_CS'], label='P_CS')
-    ax1.plot(solution_df['P_CS'] + p_l, label='P_CS + P_L')
-    # ax1.axhline(y=p_f_max, color='r', linestyle='--', label='y=4500')
-    ax1.set_xlabel('Time Period')
-    ax1.set_ylabel('Power')
-    ax1.set_title('Power Profiles')
-    ax1.legend()
-    ax1.grid(True)
-    
-    # 在第二个子图上绘制 S_t 的折线图
-    ax2.plot(solution_df['S'], label='S')
-    ax2.set_xlabel('Time Period')
-    ax2.set_ylabel('Storage State')
-    ax2.set_title('Storage State Profile')
-    ax2.legend()
-    ax2.grid(True)
-    
-    # 调整子图之间的间距
-    plt.tight_layout()
-    # 保存图片
-    plt.savefig("power_and_storage_profiles.png")
-    
-    # 显示图片
-    plt.show()
+        time_labels = generate_time_labels(num_periods)
+        # 指定字体路径
+        font_path = 'C:/Windows/Fonts/msyh.ttc'  # 微软雅黑字体路径，根据实际情况修改
+        plt.rcParams['font.family'] = ['sans-serif']
+        plt.rcParams['font.sans-serif'] = [FontProperties(fname=font_path).get_name()]
+        plt.rcParams['axes.unicode_minus'] = False  # 解决负号'-'显示为方块的问题
+
+        fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(20, 6))
+
+        # 第一个子图：功率曲线
+        ax1.plot(time_labels, p_l, label='电网侧优化前功率')
+        ax1.plot(time_labels, solution_df['P_CS'], label='储能运行功率')
+        ax1.plot(time_labels, solution_df['P_CS'] + p_l, label='电网侧实际功率')
+        ax1.axhline(y=p_f_max, color='r', linestyle='--', label=f'需量控制阈值={p_f_max:.2f} kW', lw=0.7, alpha=0.7)
+        ax1.xaxis.set_major_locator(mdates.HourLocator(interval=1))  # 每1小时标记一次
+        ax1.xaxis.set_major_formatter(mdates.DateFormatter('%H:%M'))  # 格式化时间标签为小时:分钟
+        ax1.set_xlabel('时刻')
+        ax1.set_ylabel('功率 (kW)')
+        ax1.set_title('功率曲线')
+        ax1.legend()
+        ax1.grid(True)
+        ax1.xaxis.set_major_locator(mdates.HourLocator(interval=1))
+        ax1.xaxis.set_major_formatter(mdates.DateFormatter('%H:%M'))
+        ax1.tick_params(axis='x', rotation=45)  # 设置横坐标标签旋转45度
+
+        # 第二个子图：储能电量曲线
+        ax2.plot(time_labels, solution_df['S'], label='储能电量')
+        ax2.xaxis.set_major_locator(mdates.HourLocator(interval=1))
+        ax2.xaxis.set_major_formatter(mdates.DateFormatter('%H:%M'))
+        ax2.set_xlabel('时刻')
+        ax2.set_ylabel('储能电量 (kWh)')
+        ax2.set_title('储能电量曲线')
+        ax2.legend()
+        ax2.grid(True)
+        ax2.xaxis.set_major_locator(mdates.HourLocator(interval=1))
+        ax2.xaxis.set_major_formatter(mdates.DateFormatter('%H:%M'))
+        ax2.tick_params(axis='x', rotation=45)  # 设置横坐标标签旋转45度
+
+        # 调整子图间距和显示格式
+        plt.tight_layout()
+        plt.xticks(rotation=45)  # 如果时间标签重叠，可以旋转标签
+        # plt.show()
+        plt.savefig(figpath)
+
+        return prob.solution.get_status(),prob.solution.get_objective_value(), solution_df
+    except cplex.exceptions.CplexError as exc:
+        print(exc)
+        return None, None, None
 
 if __name__ == "__main__":
-    solve_problem("C:\\Codes\\Projects\\Demand_management_verify\\Data\\xr_12_filtered.csv", s_n=1800, alpha=0.85, delta_t=0.25, output_file="C:\\Codes\\Projects\\Demand_management_verify\\Data\\daily_opt.csv")
+    status, cost, solution = solve_problem("C:\\Codes\\Projects\\Demand_management_verify\\Data\\xr_12_filtered.csv", s_n=1800, alpha=0.85, delta_t=0.25, output_file="C:\\Codes\\Projects\\Demand_management_verify\\Data\\daily_opt.csv")
+    print("Cost:", cost)
+    print("Status:", status)
---
+++ daily_pred.py
@@ -0,0 +1,62 @@
+import daily_opt as dopt
+import pandas as pd
+
+p_l = dopt.load_data("C:\\Codes\\Projects\\Demand_management_verify\\Data\\xr_12_filtered.csv")
+
+def calculate_threshold(p_l):
+    low = min(p_l)
+    high = max(p_l)
+    print("Lowest power demand:", low)
+    print("Highest power demand:", high)
+    tolerance = 0.1  # 阈值调整的精度
+    best_threshold = None
+    best_cost = float('inf')
+
+    while high - low > tolerance:
+        current_threshold = (high + low) / 2
+        has_solution, cost, solution = dopt.solve_problem("C:\\Codes\\Projects\\Demand_management_verify\\Data\\xr_12_filtered.csv", 
+                                           s_n=1800, alpha=0.85, 
+                                           delta_t=0.25, 
+                                           output_file="C:\\Codes\\Projects\\Demand_management_verify\\Data\\daily_pred.csv",
+                                           p_f_max=current_threshold)
+
+        if has_solution:
+            if cost < best_cost:
+                best_cost = cost
+                best_threshold = current_threshold
+                high = current_threshold  # 成本更低，试图通过降低上限来找到更优阈值
+            else:
+                low = current_threshold  # 当前成本不佳，提高下限
+        else:
+            # 如果没有解，意味着当前阈值过低或其他原因导致无法满足条件
+            # 提高下限试图找到可行解
+            low = current_threshold
+
+    return best_threshold if best_threshold is not None else 'No feasible solution found within the given range'
+
+def grid_cost(p_l, is_valley):
+    cost = 0
+    m_valley = 0.389  # m值在谷时段
+    m_non_valley = 0.656  # m值在非谷时段
+    for p in p_l:
+        if is_valley:
+            cost += m_valley * p * 0.25
+        else:
+            cost += m_non_valley * p * 0.25
+    return cost
+
+# 使用历史负载数据调用此函数
+num_periods = len(p_l)
+threshold = calculate_threshold(p_l)
+print("Optimal threshold for the day:", threshold)
+has_solution, cost, solution = dopt.solve_problem(file_path="C:\\Codes\\Projects\\Demand_management_verify\\Data\\xr_12_filtered.csv", 
+                                    s_n=1800, alpha=0.85, 
+                                    delta_t=0.25, 
+                                    output_file="C:\\Codes\\Projects\\Demand_management_verify\\Data\\daily_pred.csv",
+                                    p_f_max=threshold,
+                                    figpath="C:\\Codes\\Projects\\Demand_management_verify\\Data\\daily_pred_curve.png")
+print("Cost:", cost)
+
+is_valley = [True if (i>=36 and i<=48) or (i>=68 and i<=76) or (i>=80 and i<=88) else False for i in range(num_periods)]
+print("Original cost:", grid_cost(p_l, is_valley))
+print("Now cost:", grid_cost(p_l+solution['P_CS'], is_valley))
